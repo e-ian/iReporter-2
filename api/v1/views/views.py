28f365face726b0c18e2cb0dc322fc6f09c538aa
e-ian <e-ian>
2018-12-18 16:24:57 +0300

    adds default home route view

--- api/v1/views/views.py
+++ api/v1/views/views.py
@@ -1,2 +1,8 @@
 from flask import jsonify, make_response, request
 from api.v1 import app
+
+@app.route('/')
+def home():
+    """default route for homepage"""
+
+    return jsonify({'message': 'Welcome to iReporter'})
