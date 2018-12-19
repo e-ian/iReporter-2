0a96f9bb7fb6313ea53c24ccb1d3e49f8eb24ca7
e-ian <e-ian>
2018-12-20 01:44:33 +0300

    adds tests for the api endpoints

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,6 +4,7 @@ import unittest
 import json
 from api.v1 import app
 from api.v1.views import views
+from api.v1.models.redflags import Redflags
 
 class TestUser(unittest.TestCase):
     """class for testing the API endpoints"""
@@ -20,6 +21,40 @@ class TestUser(unittest.TestCase):
         with self.client as client:
             response = client.get('/')
             self.assertEqual(response.status_code, 200)
+
+    def test_create_redflag(self):
+        """ tests if a redflag has been created """
+
+        redflag = {
+            'createdOn' : '2018-12-19',
+            'createdBy' : 'emma',
+            'incidenttype' : 'redflag',
+            'location' : 'mulago',
+            'status' : 'resolved',
+            'Images' : 'Images',
+            'Videos' :'Videos',
+            'comment' :'bribery in OPM'}
+
+        with self.client as client:
+            response = client.post('/api/v1/redflags', data=json.dumps(redflag), \
+            content_type='application/json')
+            self.assertEqual(response.status_code, 200)
+
+    def test_get_all_redflags(self):
+        """tests if all redflags can be fetched """
+        with self.client as client:
+            response = client.get('/api/v1/redflags')
+            self.assertEqual(response.status_code, 200)
+
+    def test_get_specific_redflag(self):
+        """ tests if a specific redflag can be fetched """
+        with self.client as client:
+            response = client.get('/api/v1/redflags/1')
+            self.assertEqual(response.status_code, 200)
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
