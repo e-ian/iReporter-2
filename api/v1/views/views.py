819ae334c83255fd7c5331bd8894be8e09c95d93
e-ian <e-ian>
2018-12-19 15:12:44 +0300

    modifies routes to include get all redflags [Delivers #162743167]

--- api/v1/views/views.py
+++ api/v1/views/views.py
@@ -39,3 +39,10 @@ def create_redflag():
                 return make_response(jsonify({"status" : 201, "data" : [{"id": int(id), "message": "Created red-flag record"}]}))
     except Exception:
                 return make_response(jsonify({"error": "Invalid input format"}), 400)
+
+@app.route('/api/v1/redflags', methods=['GET'])
+def get_redflags():
+    """ method implementing get all redflags endpoint """
+    all_redflags = Redflags.get_all_redflags()
+    if all_redflags:
+        return make_response(jsonify({'status': 200, 'redflag_list':all_redflags}))
