faa6726924a9dda4d20251271bf348345f19600c
e-ian <e-ian>
2018-12-19 14:58:26 +0300

    modifies Redflags class

--- api/v1/models/Redflags.py
+++ api/v1/models/Redflags.py
@@ -9,15 +9,15 @@ class Redflags:
 
     redflag_list = []
 
-    def __init__(self, *args):
-        self.createdOn = args[0]
-        self.createdBy = args[1]
-        self.type = args[2]
-        self.location = args[3]
-        self.status = args[4]
-        self.Images = args[5]
-        self.Videos = args[6]
-        self.comment = args[7]
+    def __init__(self, createdOn, createdBy, incidenttype, location, status, Images, Videos, comment):
+        self.createdOn = createdOn
+        self.createdBy = createdBy
+        self.incidenttype = incidenttype
+        self.location = location
+        self.status = status
+        self.Images = Images
+        self.Videos = Videos
+        self.comment = comment
 
     @classmethod
     def len_of_redflag_dict(cls):
