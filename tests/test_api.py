f87684d97f3e77063fee02dccb354c456683c9c4
e-ian <e-ian>
2019-01-17 00:05:22 +0300

    Styling with PEP8

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,24 +5,33 @@ import json
 from api.v1 import app
 from api.v1.views import views
 from api.v1.models.Redflags import Redflags
-from . import (redflag, redflagempty, invalid_date, empty_list, redflag_comment, empty_incident_type, empty_image)
+from . import (
+    redflag,
+    redflagempty,
+    invalid_date,
+    empty_list,
+    redflag_comment,
+    empty_incident_type,
+    empty_image)
+
 
 class TestUser(unittest.TestCase):
     """class for testing the API endpoints"""
+
     def setUp(self):
-        """ 
+        """
         method that:
         sets up instance of app for the tests
         creates client to run the tests
         """
         kwargs = {
-        'createdOn' : '2018-12-19',
-        'createdBy' : 'emma',
-        'incidenttype' : 'redflag',
-        'location' : 'mulago',
-        'status' : 'resolved',
-        'Images' : 'Images',
-        'comment' :'bribery in OPM'
+            'createdOn': '2018-12-19',
+            'createdBy': 'emma',
+            'incidenttype': 'redflag',
+            'location': 'mulago',
+            'status': 'resolved',
+            'Images': 'Images',
+            'comment': 'bribery in OPM'
         }
         self.redflags = Redflags(**kwargs)
         self.client = app.test_client()
@@ -37,23 +46,35 @@ class TestUser(unittest.TestCase):
     def test_create_redflag(self):
         """ tests if a redflag has been created """
         with self.client as client:
-            response = client.post('/api/v1/redflags', data=json.dumps(redflag), content_type='application/json')
+            response = client.post(
+                '/api/v1/redflags',
+                data=json.dumps(redflag),
+                content_type='application/json')
             self.assertEqual(response.status_code, 201)
             self.assertIn("Created red-flag record", str(response.data))
 
     def test_empty_input_redflag(self):
         """ tests if one of the inputs of a redflag is empty """
         with self.client as client:
-            response = client.post('/api/v1/redflags', data=json.dumps(redflagempty), content_type='application/json')
+            response = client.post(
+                '/api/v1/redflags',
+                data=json.dumps(redflagempty),
+                content_type='application/json')
             self.assertEqual(response.status_code, 400)
-            self.assertIn("comment and location fields cannot be empty and comment takes alphabets", str(response.data))
+            self.assertIn(
+                "comment and location fields cannot be empty and comment takes alphabets", str(
+                    response.data))
 
     def test_invalid_date_format(self):
         """ tests the inputs of the date """
         with self.client as client:
-            response = client.post('/api/v1/redflags', data=json.dumps(invalid_date), content_type='application/json')
+            response = client.post(
+                '/api/v1/redflags',
+                data=json.dumps(invalid_date),
+                content_type='application/json')
             self.assertEqual(response.status_code, 400)
-            self.assertIn("Incorrect date format, should be YYYY-MM-DD", str(response.data))
+            self.assertIn(
+                "Incorrect date format, should be YYYY-MM-DD", str(response.data))
 
     def test_get_all_redflags(self):
         """tests if all redflags can be fetched """
@@ -70,34 +91,60 @@ class TestUser(unittest.TestCase):
     def test_specific_redflag_not_found(self):
         """tests is a red flag cannot be got by flagid"""
         with self.client as client:
-            client.post('/api/v1/redflags', json=dict(createdBy='emma', incidenttype='redflag', location='mulago', status='resolved', Images='image', comment='bribe'))
+            client.post(
+                '/api/v1/redflags',
+                json=dict(
+                    createdBy='emma',
+                    incidenttype='redflag',
+                    location='mulago',
+                    status='resolved',
+                    Images='image',
+                    comment='bribe'))
             response = client.get('/api/v1/redflags/10000')
             self.assertIn("Redflag not found", str(response.data))
 
     def test_edit_redflag_location(self):
         """ tests for editing location """
         with self.client as client:
-            client.post('api/v1/redflags', data=json.dumps(redflag), content_type='application/json')
-            response = client.patch('/api/v1/redflags/1/location', json=dict(location='kampala'))
+            client.post(
+                'api/v1/redflags',
+                data=json.dumps(redflag),
+                content_type='application/json')
+            response = client.patch(
+                '/api/v1/redflags/1/location',
+                json=dict(
+                    location='kampala'))
             self.assertEqual(response.status_code, 200)
 
     def test_edit_location_not_found(self):
         """ tests if the flagid doesnot exit when editing location """
         with self.client as client:
-            response = client.patch('api/v1/redflags/0/location', json=dict(location='kampala'))
+            response = client.patch(
+                'api/v1/redflags/0/location',
+                json=dict(
+                    location='kampala'))
             self.assertIn('Redflag not found', str(response.data))
 
     def test_edit_redflag_comment(self):
         """ tests for editing comment"""
         with self.client as client:
-            client.post('api/v1/redflags', data=json.dumps(redflag), content_type='application/json')
-            response = client.patch('/api/v1/redflags/1/comment', json=dict(comment='bribery in OPM'))
+            client.post(
+                'api/v1/redflags',
+                data=json.dumps(redflag),
+                content_type='application/json')
+            response = client.patch(
+                '/api/v1/redflags/1/comment',
+                json=dict(
+                    comment='bribery in OPM'))
             self.assertEqual(response.status_code, 200)
 
     def test_edit_comment_not_found(self):
         """ tests if the flagid doesnot exit when editing comment """
         with self.client as client:
-            response = client.patch('api/v1/redflags/0/comment', json=dict(comment='bribery'))
+            response = client.patch(
+                'api/v1/redflags/0/comment',
+                json=dict(
+                    comment='bribery'))
             self.assertIn('Redflag not found', str(response.data))
 
     def test_deleted_red_flag(self):
@@ -109,16 +156,26 @@ class TestUser(unittest.TestCase):
     def test_incident_type(self):
         """ tests if incident type is not empty """
         with self.client as client:
-            response = client.post("api/v1/redflags", data=json.dumps(empty_incident_type), content_type='application/json')
+            response = client.post(
+                "api/v1/redflags",
+                data=json.dumps(empty_incident_type),
+                content_type='application/json')
             self.assertEqual(response.status_code, 400)
-            self.assertIn('incidenttype or status cannot have empty spaces', str(response.data))
-    
+            self.assertIn(
+                'incidenttype or status cannot have empty spaces', str(
+                    response.data))
+
     def test_empty_image(self):
         """ tets if image field is not empty """
         with self.client as client:
-            response = client.post("api/v1/redflags", data=json.dumps(empty_image), content_type='application/json')
+            response = client.post(
+                "api/v1/redflags",
+                data=json.dumps(empty_image),
+                content_type='application/json')
             self.assertEqual(response.status_code, 400)
-            self.assertIn('The status and image fields cannot be empty', str(response.data))
+            self.assertIn(
+                'The status and image fields cannot be empty', str(
+                    response.data))
 
     # def test_edit_comment_payload(self):
     #     """ tests for payload data of edit comment """
@@ -128,6 +185,7 @@ class TestUser(unittest.TestCase):
     #         response_data = json.loads(response.data.decode())
     #         self.assertEqual(response.status_code, 200)
     #         self.assertEqual("Updated redflag's location", response_data['message'])
+
 
 if __name__ == "__main__":
     unittest.main()
