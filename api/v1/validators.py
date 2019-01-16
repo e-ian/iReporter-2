a83934ed7443abe470d57a7ee737d4f69d7d80a6
e-ian <e-ian>
2019-01-16 13:41:14 +0300

    Adds validations for the inputs

--- api/v1/validators.py
+++ api/v1/validators.py
@@ -9,9 +9,9 @@ import datetime
 class Validators:
 
     @staticmethod
-    def validate_input_string(incidenttype, location, status, comment):
+    def validate_input_string(incidenttype, status):
         """ method to validate if the input is a string and not empty """
-        if not incidenttype or not location or not status or not comment:
+        if not incidenttype or not status:
             return False
         else:
             return True
@@ -22,5 +22,24 @@ class Validators:
         try:
             datetime.datetime.strptime(createdOn, '%Y-%m-%d')
         except ValueError:
-            return jsonify({'error': 'Incorrect data format, should be YYYY-MM-DD'}), 400
+            return jsonify({'error': 'Incorrect date format, should be YYYY-MM-DD'}), 400
+
+    @staticmethod
+    def validate_comment_and_location(comment, location):
+        """ method to validate comment and location inputs """
+        if not comment or not location or comment.isspace() or location.isspace() \
+        or not isinstance(comment, str):
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def validate_status_and_image(status, images):
+        """ method to validate status and image inputs """
+        if not status or not images or status.isspace() or images.isspace():
+            return False
+        else:
+            return True
+        
+
         
