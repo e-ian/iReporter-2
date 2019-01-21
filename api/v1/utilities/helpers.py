1fc2c3bd2a4c19619f425e5397a2018fd0e6b929
e-ian <e-ian>
2019-01-22 00:12:37 +0300

    modifies helpers.py

--- api/v1/utilities/helpers.py
+++ api/v1/utilities/helpers.py
@@ -9,12 +9,12 @@ def update_redflags(redflag_id, column, column_data):
     flag_list = red.get_specific_redflag(redflag_id)
     if flag_list:
         red.edit_redflags(redflag_id, column, column_data)
-        return "Updated redflag's {}".format(column), 200
+        return "Updated redflags {}".format(column), 200
     return "Redflag not found in records", 404
 
 def patch_interventions(intervention_id, column, column_data):
     incid_list = incid.get_single_intervention(intervention_id)
     if incid_list:
         incid.edit_interventions(intervention_id, column, column_data)
-        return "Updated intervention's {}".format(column), 200
+        return "Updated interventions {}".format(column), 200
     return "Intervention record not found", 404
