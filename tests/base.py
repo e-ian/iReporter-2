fc62ad1b79282a6c87bd4fd8501f606acd54ba10
e-ian <e-ian>
2019-01-22 01:28:58 +0300

    adds base tests for interventions

--- tests/base.py
+++ tests/base.py
@@ -8,7 +8,10 @@ from api.v1.models import Database
 import re
 from tests import(redflag, empty_incident_type, empty_comment, empty_image, invalid_redflag, edit_redflag_location, \
 edit_redflag_location_not_found, edit_invalid_redflag_location, edit_redflag_comment, edit_redflag_comment_not_found, \
-edit_invalid_redflag_comment, edit_redflag_status, edit_redflag_status_not_found, edit_invalid_redflag_status)
+edit_invalid_redflag_comment, edit_redflag_status, edit_redflag_status_not_found, edit_invalid_redflag_status, \
+intervention, empty_incident_type, empty_comment_int, empty_image_int, invalid_intervention, \
+edit_int_location, edit_invalid_int_location, edit_int_comment, edit_invalid_int_comment, edit_int_status, \
+edit_invalid_int_status)
 
 app.config.from_object(TestingConfig)
 
@@ -23,6 +26,8 @@ class TestUser(unittest.TestCase):
         self.db.cur.execute("DROP TABLE redflags CASCADE")
         self.db.cur.execute("DROP TABLE interventions CASCADE")
         self.db.cur.execute("DROP TABLE users CASCADE")
+
+    """ base tests for redflags """
 
     def create_flag(self, redflag):
         response = self.client.post("/api/v1/redflags", \
@@ -118,6 +123,100 @@ class TestUser(unittest.TestCase):
     def home_route(self):
         response = self.client.get('/')
         return response
+
+    """ base tests for interventions """
+    
+    def create_intervention(self, intervention):
+        response = self.client.post("/api/v1/interventions", \
+        data=json.dumps(intervention), content_type='application/json')
+        return response
+
+    def empty_incident_type(self, empty_incident_type):
+        response = self.client.post("/api/v1/interventions", \
+        data=json.dumps(empty_incident_type), content_type='application/json')
+        return response
+
+    def int_comment_empty(self, empty_comment_int):
+        response = self.client.post("/api/v1/interventions", \
+        data=json.dumps(empty_comment_int), content_type='application/json')
+        return response
+
+    def int_image_empty(self, empty_image_int):
+        response = self.client.post("/api/v1/interventions", \
+        data=json.dumps(empty_image_int), content_type='application/json')
+        return response
+
+    def duplicate_intervention(self, intervention):
+        response = self.client.post("/api/v1/interventions", \
+        data=json.dumps(intervention), content_type='application/json')
+        return response
+
+    def invalid_intervention(self, invalid_intervention):
+        response = self.client.post("/api/v1/interventions", \
+        data=json.dumps(invalid_intervention), content_type='application/json')
+        return response
+
+    def get_interventions(self):
+        response = self.client.get("/api/v1/interventions")
+        return response
+
+    def get_single_intervention(self):
+        response = self.client.get("/api/v1/interventions/1")
+        return response
+
+    def intervention_not_found(self):
+        response = self.client.get("/api/v1/interventions/10000")
+        return response
+
+    def delete_intervention(self):
+        response = self.client.delete("/api/v1/interventions/1")
+        return response
+
+    def edit_int_location(self, edit_int_location):
+        response = self.client.patch("/api/v1/interventions/1/location", \
+        data=json.dumps(edit_int_location), content_type='application/json')
+        return response
+
+    def edit_int_location_not_found(self, edit_int_location):
+        response = self.client.patch("/api/v1/interventions/10000/location", \
+        data=json.dumps(edit_int_location), content_type='application/json')
+        return response
+
+    def edit_invalid_int_location(self, edit_invalid_int_location):
+        response = self.client.patch("/api/v1/interventions/1/location", \
+        data=json.dumps(edit_invalid_int_location), content_type='application/json')
+        return response
+
+    def edit_int_comment(self, edit_int_comment):
+        response = self.client.patch("/api/v1/interventions/1/comment", \
+        data=json.dumps(edit_int_comment), content_type='application/json')
+        return response
+
+    def edit_int_comment_not_found(self, edit_int_comment):
+        response = self.client.patch("/api/v1/interventions/10000/comment", \
+        data=json.dumps(edit_int_comment), content_type='application/json')
+        return response
+
+    def edit_invalid_int_comment(self, edit_invalid_int_comment):
+        response = self.client.patch("/api/v1/interventions/1/comment", \
+        data=json.dumps(edit_invalid_int_comment), content_type='application/json')
+        return response
+
+    def edit_int_status(self, edit_int_status):
+        response = self.client.patch("/api/v1/interventions/1/status", \
+        data=json.dumps(edit_int_status), content_type='application/json')
+        return response
+
+    def edit_int_status_not_found(self, edit_int_status):
+        response = self.client.patch("/api/v1/interventions/10000/status", \
+        data=json.dumps(edit_int_status), content_type='application/json')
+        return response
+
+    def edit_invalid_int_status(self, edit_invalid_int_status):
+        response = self.client.patch("/api/v1/interventions/1/status", \
+        data=json.dumps(edit_invalid_int_status), content_type='application/json')
+        return response
+
 
         
 
