87fc81e4dc6e40381b97325a0c4766f08f54504f
e-ian <e-ian>
2019-01-23 16:54:15 +0300

    adds tests for testing redflags

--- tests/test_redflags.py
+++ tests/test_redflags.py
@@ -1,9 +1,9 @@
 """ module tests_redflags """
 from tests.base import TestUser
-from . import(redflag, empty_incident_type, empty_comment, empty_image, invalid_redflag, \
+from . import(redflag, incident_type, empty_comment, empty_image, invalid_redflag, \
 edit_redflag_location, edit_redflag_location_not_found, edit_invalid_redflag_location, \
 edit_redflag_comment, edit_redflag_comment_not_found, edit_invalid_redflag_comment, edit_redflag_status, \
-edit_redflag_status_not_found, edit_invalid_redflag_status)
+edit_redflag_status_not_found, edit_invalid_redflag_status, redflag_draft, empty_flag_location)
 
 class TestApp(TestUser):
 
@@ -12,19 +12,29 @@ class TestApp(TestUser):
         self.assertIn("Created red-flag record", str(response.data))
         self.assertEqual(response.status_code, 201)
 
-    def test_empty_incident_type(self):
-        response = self.empty_type(empty_incident_type)
-        self.assertIn('incident_type or status cannot have empty spaces', str(response.data))
+    def test_incident_type(self):
+        response = self.empty_type(incident_type)
+        self.assertIn('incident type can only be redflag', str(response.data))
         self.assertEqual(response.status_code, 400)
 
     def test_empty_comment(self):
         response = self.comment_empty(empty_comment)
-        self.assertIn('comment and location fields cannot be empty and comment takes alphabets', str(response.data))
+        self.assertIn('comment field should be a string and should contain alphabets', str(response.data))
+        self.assertEqual(response.status_code, 400)
+
+    def test_redflag_draft(self):
+        response = self.redflag_draft(redflag_draft)
+        self.assertIn('status can only be posted as a draft', str(response.data))
+        self.assertEqual(response.status_code, 400)
+
+    def test_empty_flag_location(self):
+        response = self.empty_flag_location(empty_flag_location)
+        self.assertIn('location field cannot be empty', str(response.data))
         self.assertEqual(response.status_code, 400)
 
     def test_empty_image(self):
         response = self.image_empty(empty_image)
-        self.assertIn('The status and image fields cannot be empty', str(response.data))
+        self.assertIn('The image field cannot be empty', str(response.data))
         self.assertEqual(response.status_code, 400)
 
     def test_duplicate_redflag(self):
