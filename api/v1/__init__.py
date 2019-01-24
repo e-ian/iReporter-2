9c872c636bb6c286713fb7901b41217d0a85ce82
e-ian <e-ian>
2019-01-24 10:37:00 +0300

    refactor code

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,10 +1,8 @@
 from flask import Flask
-from flask_jwt_extended import (JWTManager, jwt_required, create_access_token, get_jwt_identity)
 from api.v1.dbhandler import Database
 
 app = Flask(__name__)
-app.config['JWT_SECRET_KEY'] = 'apaclango'
-jwt = JWTManager(app)
+
 db_connect = Database()
 from api.v1.views import redflags_views
 from api.v1.views import interventions_views
