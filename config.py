254cd26f4a740b34790fa3d06cb0df2429779710
e-ian <e-ian>
2019-01-25 15:49:10 +0300

    modifies config file

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class ProductionConfig(Config):
     """
     class for Production configuration
     """
-    DEBUG = True
+    DEBUG = False
     DATABASE_URI = "postgres://bmwdqpkfurzqev:b4ee0a501e7c5132bc0b157a36436af9696f2cb1e4470d53503c2aefd953ee8f@ec2-54-83-50-174.compute-1.amazonaws.com:5432/da51826nqt75f"
 
 app_config = {
