637fe9e989c30d4ab747604d3f15d5927c5e8d5a
e-ian <e-ian>
2019-02-05 15:58:11 +0300

    initialises flask-cors extension in app factory

--- api/v1/__init__.py
+++ api/v1/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from api.v1.dbhandler import Database
 
 app = Flask(__name__)
+CORS(app)
 
 db_connect = Database()
 from api.v1.views import redflags_views
